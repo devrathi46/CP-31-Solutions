30761774e8d02a7b24908d71584d4faeba5cceda
devrathi46 <devrathi67155@gmal.com>
2025-05-29 23:39:50 +0530

    Add new solution to 1000_rated_problems

--- 1000_rated_problems/A_Johnny_and_Ancient_Computer.cpp
+++ 1000_rated_problems/A_Johnny_and_Ancient_Computer.cpp
@@ -58,9 +58,25 @@ int combination(int n, int k) {
 void solve() {
     int a,b;
     cin>>a>>b;
-    //*2 *4 *8 or /2 /4 /8
-    //minimum operations to convert a=b
-    
+    int cnt1=0;
+    int cnt2=0;
+    while(a%2==0){
+        a/=2;
+        cnt1++;
+    }
+    while(b%2==0){
+        b/=2;
+        cnt2++;
+    }
+    if(a!=b)cout<<-1<<endl;
+    else{
+        if(cnt1==cnt2)cout<<0<<endl;
+        else{
+            int diff=abs(cnt2-cnt1);
+            cout<<diff/3+(diff%3!=0)<<endl;
+        }
+    }
+
     
     
 }
